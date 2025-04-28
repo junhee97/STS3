bca125616729959728261e4c6bd69ada669b24c8
junhee97 <rlawnsgml97@naver.com>
2025-04-28 17:33:21 +0900

    day 06

---
+++ ex09_FILEUPDOWNLOAD/src/test/java/TxTests/Txtests.java
@@ -0,0 +1,27 @@
+package TxTests;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.app.domain.dto.MemoDto;
+import com.example.app.domain.service.MemoServiceImpl;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+
+class Txtests {
+
+	@Autowired
+	private MemoServiceImpl memoService;
+
+	@Test
+	void t1() {
+		memoService.addMemoTx(new MemoDto(9000, "a", "a", LocalDateTime.now()));
+	}
+
+}
